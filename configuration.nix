2f2d678bb9c16f55f3e543b848fc900c3c6b5d31
joseph-redmond <me@josephredmond.com>
2025-01-01 14:52:55 -0800

    Initial Commit

---
+++ configuration.nix
@@ -0,0 +1,42 @@
+{ modulesPath, config, lib, pkgs, ... }: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+
+  boot.loader.grub = {
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+
+  console.keyMap = "dvorak";
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  services.openssh = {
+    enable = true;
+  };
+
+  environment.systemPackages = [
+    pkgs.neovim
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
+
+  users.users.server_maintainer = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINB5QGHV/56stPL+hZiyWsjMyUfkvUi4PfRRf2+2VC0D josephredmondjr@Josephs-Mini.lan"
+    ];
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINB5QGHV/56stPL+hZiyWsjMyUfkvUi4PfRRf2+2VC0D josephredmondjr@Josephs-Mini.lan"
+  ];
+
+  system.stateVersion = "24.11";
+}
---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, disko, ... }: {
+    nixosConfigurations.hc001 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./configuration.nix
+        ./disk-config.nix
+      ];
+    };
+  };
+}
